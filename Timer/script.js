ba3d8e1add0c6f3025af32831141c7c5fd088484
Auau2401 <auau2401@gmail.com>
2023-11-28 21:16:25 +0100

    Update script.js

--- Timer/script.js
+++ Timer/script.js
@@ -1,10 +1,24 @@
-const endDate = new Date("2023-11-28 23:59:59").getTime();
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const timestamp = urlParams.get('timestamp') * 1000
+const endDate = new Date(timestamp);
+const now = new Date().getTime();
+const distance = endDate - now;
+
+
+
 
 import codes from './codes.json' assert { type: 'json' };;
 
 const timer = setInterval(function() {
 	const now = new Date().getTime();
 	const distance = endDate - now;
+
+	if (typeof timestamp !== 'number' || distance >= 44672917835){
+		clearInterval(timer);
+		alert("Ungültiger Timestamp! / Die Zeitspanne ist zu groß!");
+		return;
+	}
 
 	var all_ele = document.body.getElementsByTagName("*");
 	for (const element of all_ele) {
@@ -13,17 +27,15 @@ const timer = setInterval(function() {
 
 	if (distance <= 0) {
 		clearInterval(timer);
-		document.getElementById("countdown").innerHTML = "Countdown abgelaufen!";
+		alert("Countdown abgelaufen!");
 	} else {
 		const days = Math.floor(distance / (1000 * 60 * 60 * 24));
 		const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 		const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 		const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-		document.getElementById("countdown").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-
 		if (days != 0){
-			if (seconds % 2 === 0){
+			if (seconds % 2 != 0){
 				var el_39 = document.getElementById("39");
 
         		el_39.classList.add("active");
@@ -37,7 +49,7 @@ const timer = setInterval(function() {
 		}
 
 		if (hours != 0){
-			if (seconds % 2 === 0){
+			if (seconds % 2 != 0){
 				var el_65 = document.getElementById("65");
 
         		el_65.classList.add("active");
@@ -50,8 +62,8 @@ const timer = setInterval(function() {
 
 		}
 
-		if (minutes != 0 && seconds != 0){
-			if (seconds % 2 === 0){
+		if (minutes != 0){
+			if (seconds % 2 != 0){
 				var el_94 = document.getElementById("94");
 
         		el_94.classList.add("active");
@@ -75,6 +87,21 @@ const timer = setInterval(function() {
 			const minutes_array = codes["minutes"]["" + minutes].split("+");
 
 		for (const element of minutes_array) {
+			document.getElementById(element).classList.add("active");
+		  }
+	    }
+
+		if (hours != 0){
+			const hours_array = codes["hours"]["" + hours].split("+");
+
+		for (const element of hours_array) {
+			document.getElementById(element).classList.add("active");
+		  }
+	    }
+		if (days != 0){
+			const days_array = codes["days"]["" + days].split("+");
+
+		for (const element of days_array) {
 			document.getElementById(element).classList.add("active");
 		  }
 	    }
